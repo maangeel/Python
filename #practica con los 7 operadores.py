27f70877d8b2b3075a08cc28f0122cf13e960997
maangeel <miguel.fernandez@campus.xaloc.org>
2025-09-16 10:10:23 +0200

    Add files via upload

--- #practica con los 7 operadores.py
+++ #practica con los 7 operadores.py
@@ -1,59 +1,67 @@
 #practica con los 7 operadores
 #calculadora
 
-print("Hay 7 operadores en python: suma, resta, multiplicacion, division, division exacta, modulo y potencia")
-operador = input("Escoge un tipo de Operación: ")
 
-if (operador=="suma"):
-    print("Has seleccionado suma.")
-    valor1 = float(input("A continuación escoge el primer valor: "))
-    valor2 = float(input("Escoge un segundo valor: "))
-    resultado = valor1+valor2
-    print(f"La suma de {valor1} + {valor2} da: {resultado}")
+while True:
+    print("Has iniciado la calculadora, para finalizar escribe la palabra: finalizar")
+    print("Hay 7 operadores en python: suma, resta, multiplicacion, division, division exacta, modulo y potencia")
+    operador = input("Escoge un tipo de Operación: ")
 
-elif (operador=="resta"):
-    print("Has seleccionado resta.")
-    valor1 = float(input("A continuación escoge el primer valor: "))
-    valor2 = float(input("Escoge un segundo valor: "))
-    resultado = valor1-valor2
-    print(f"La resta de {valor1} - {valor2} da: {resultado}")
+    if (operador=="finalizar"):
+        print("El programa se ha cerrado correctamente")
+        break
 
-elif (operador=="multiplicacion"):
-    print("Has seleccionado multiplicación.")
-    valor1 = float(input("A continuación escoge el primer valor: "))
-    valor2 = float(input("Escoge un segundo valor: "))
-    resultado = valor1*valor2
-    print(f"La multiplicación de {valor1} * {valor2} da: {resultado}")
+    if (operador=="suma"):
+        print("Has seleccionado suma.")
+        valor1 = float(input("A continuación escoge el primer valor: "))
+        valor2 = float(input("Escoge un segundo valor: "))
+        resultado = valor1+valor2
+        print(f"La suma de {valor1} + {valor2} da: {resultado}")
 
-elif (operador=="division"):
-    print("Has seleccionado división.")
-    valor1 = float(input("A continuación escoge el primer valor: "))
-    valor2 = float(input("Escoge un segundo valor: "))
-    resultado = valor1/valor2
-    print(f"La división de {valor1} / {valor2} da: {resultado}")
+    elif (operador=="resta"):
+        print("Has seleccionado resta.")
+        valor1 = float(input("A continuación escoge el primer valor: "))
+        valor2 = float(input("Escoge un segundo valor: "))
+        resultado = valor1-valor2
+        print(f"La resta de {valor1} - {valor2} da: {resultado}")
 
-elif (operador=="division exacta"):
-    print("Has seleccionado división exacta.")
-    valor1 = float(input("A continuación escoge el primer valor: "))
-    valor2 = float(input("Escoge un segundo valor: "))
-    resultado = valor1//valor2
-    print(f"La división exacta de {valor1} / {valor2} da: {resultado}")
+    elif (operador=="multiplicacion"):
+        print("Has seleccionado multiplicación.")
+        valor1 = float(input("A continuación escoge el primer valor: "))
+        valor2 = float(input("Escoge un segundo valor: "))
+        resultado = valor1*valor2
+        print(f"La multiplicación de {valor1} * {valor2} da: {resultado}")
 
-elif (operador=="modulo"):
-    print("Has seleccionado módulo.")
-    valor1 = float(input("A continuación escoge el primer valor: "))
-    valor2 = float(input("Escoge un segundo valor: "))
-    resultado = valor1-valor2
-    print(f"El módulo de {valor1} / {valor2} da: {resultado}")
+    elif (operador=="division"):
+        print("Has seleccionado división.")
+        valor1 = float(input("A continuación escoge el primer valor: "))
+        valor2 = float(input("Escoge un segundo valor: "))
+        resultado = valor1/valor2
+        print(f"La división de {valor1} / {valor2} da: {resultado}")
 
-elif (operador=="potencia"):
-    print("Has seleccionado potencia.")
-    valor1 = float(input("A continuación escoge el primer valor: "))
-    valor2 = float(input("Escoge un segundo valor: "))
-    resultado = valor1%valor2
-    print(f"{valor1} elevado a {valor2} da: {resultado}")
+    elif (operador=="division exacta"):
+        print("Has seleccionado división exacta.")
+        valor1 = float(input("A continuación escoge el primer valor: "))
+        valor2 = float(input("Escoge un segundo valor: "))
+        resultado = valor1//valor2
+        print(f"La división exacta de {valor1} / {valor2} da: {resultado}")
 
-#mensaje de error en caso de no coincidir con ningun operador
-else:
-    print("No has escrito correctamente el operador.")
-    print("Asegúrate de escribirlo sin acentos ni mayúsculas.")
+    elif (operador=="modulo"):
+        print("Has seleccionado módulo.")
+        valor1 = float(input("A continuación escoge el primer valor: "))
+        valor2 = float(input("Escoge un segundo valor: "))
+        resultado = valor1%valor2
+        print(f"El módulo de {valor1} / {valor2} da: {resultado}")
+
+    elif (operador=="potencia"):
+        print("Has seleccionado potencia.")
+        valor1 = float(input("A continuación escoge el primer valor: "))
+        valor2 = float(input("Escoge un segundo valor: "))
+        resultado = valor1**valor2
+        print(f"{valor1} elevado a {valor2} da: {resultado}")
+
+    #mensaje de error en caso de no coincidir con ningun operador
+    else:
+        print("No has escrito correctamente el operador.")
+        print("Asegúrate de escribirlo sin acentos ni mayúsculas.")
+
