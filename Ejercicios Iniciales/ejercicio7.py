03b99a5e0e1e2ec198d3ec56618ad7b1e18fac1e
maangeel <miguel.fernandez@campus.xaloc.org>
2025-09-17 09:53:46 +0200

    Create ejercicio7.py

---
+++ Ejercicios Iniciales/ejercicio7.py
@@ -0,0 +1,12 @@
+#7. programa que calcule dos operandos con los 7 operadores vistos en clase.
+# ¿Cómo puedes forzar que el resultado de la división tenga 2 decimales?
+
+#inputs
+var1 = float(input("Introduce un valor: "))
+var2 = float(input("Introduce otro valor: "))
+
+#suma
+suma = var1 + var2
+print(f"El resultado de la suma de {var1} + {var2} es {suma}")
+
+#resta
