ac34881052c159c1864ac937eb6cea71e21d40a9
maangeel <miguel.fernandez@campus.xaloc.org>
2025-09-17 09:41:37 +0200

    python

---
+++ Ejercicios Iniciales/ejercicio3.py
@@ -0,0 +1,11 @@
+#3. Programa que pida dos números enteros y realice la suma correspondiente
+
+#entrada de input
+var1 = int(input("Introduce un valor entero: "))
+var2 = int(input("Introduce otro valor entero: "))
+
+#suma
+result = var1+var2
+
+#print de la suma
+print(f"El total de la suma es de: {result}")
---
+++ Ejercicios Iniciales/ejercicio4.py
@@ -0,0 +1,12 @@
+#4. A partir del código anterior
+#realiza una versión para números con decimales
+
+#entrada de input
+var1 = float(input("Introduce un número decimal: "))
+var2 = float(input("Introduce otro número decimal: "))
+
+#suma
+result = var1+var2
+
+#print de la suma
+print(f"El total de la suma es de: {result}")
