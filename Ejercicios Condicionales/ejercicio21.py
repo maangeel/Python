62a2b54e70b23f88e2f9fdaa0f2e6a0a001300ce
maangeel <miguel.fernandez@campus.xaloc.org>
2025-09-30 10:47:07 +0200

    Update ejercicio21.py

--- Ejercicios Condicionales/ejercicio21.py
+++ Ejercicios Condicionales/ejercicio21.py
@@ -2,16 +2,20 @@
 #que el valor de la raíz cuadrada no de un valor negativo
 import math
 
-a = int(input("Introduce el valor de a: "))
-b = int(input("Introduce el valor de b: "))
-c = int(input("Introduce el valor de c: "))
+a = float(input("Introduce el valor de a: "))
+b = float(input("Introduce el valor de b: "))
+c = float(input("Introduce el valor de c: "))
 
 #comprobar si la ecuación se puede hacer o no
-ec1 = b**2-(4*a*c)
-x1 = (-b+(math.pi(b**2+(4*a*c))))/(2*a)
-x2 = (-b+(math.pi(b**2-(4*a*c))))/(2*a)
+ec1 = (b**2-(4*a*c))
+
 
 if ec1 < 0:
     print("La raíz no puede ser un valor negativo.")
 
 else:
+    raiz = ec1**0,5
+    x1 = (-b+raiz)/(2*a)
+    x2 = (-b-raiz)/(2*a)
+    print(f"El valor de x1 es de: {x1}")
+    print(f"El valor de x2 es de: {x2}")
