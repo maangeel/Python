06be48fb6b45add76ff92ad17355c59b08257608
maangeel <miguel.fernandez@campus.xaloc.org>
2025-10-08 09:06:43 +0200

    Update ejercicio33.py

--- Ejercicios Condicionales/ejercicio33.py
+++ Ejercicios Condicionales/ejercicio33.py
@@ -6,4 +6,5 @@ vocales = ["a","á","à","e","é","è","i","í","ì","o","ó","ò","u","ú","ù"
 
 for vocals in vocales:
     numVocals = frase.count(vocals)
-    print(f"La vocal {vocals} aparece {numVocals} veces.")
+    if numVocals != 0:
+        print(f"La vocal {vocals} aparece {numVocals} veces.")
