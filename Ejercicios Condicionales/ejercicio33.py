1eba24c91ac9152241ff1a82671d46dba1e3846a
maangeel <miguel.fernandez@campus.xaloc.org>
2025-10-08 09:02:12 +0200

    p

--- Ejercicios Condicionales/ejercicio33.py
+++ Ejercicios Condicionales/ejercicio33.py
@@ -2,7 +2,7 @@
 #hay mal que dure cien años.
 
 frase = "No hay mal que dure cien años"
-vocales = ["a", "e", "i", "o", "u"]
+vocales = ["a", "á", "à", "e", "é", "è","i", "í", "ì", "o", "ó", "ò", "u", "ú", "ù"]
 
 for vocals in vocales:
     numVocals = frase.count(vocals)
