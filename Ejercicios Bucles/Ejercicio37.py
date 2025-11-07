1bee8b4ec5787155055e9e954d93c6a267a14182
maangeel <miguel.fernandez@campus.xaloc.org>
2025-11-07 15:23:20 +0100

    Update Ejercicio37.py

--- Ejercicios Bucles/Ejercicio37.py
+++ Ejercicios Bucles/Ejercicio37.py
@@ -4,8 +4,15 @@ rep = input("Introduce el número de notas que deseas introducir: ")
 
 if rep.isdigit: #comprueba si el valor es entero
     rep = int(rep)
+    for i in range(rep): #hace tantas repeticiones como el usuario pida
+        nota = float(input("Introduce tu nota sobre 10: "))
 
+        if nota>=5 and nota<=10:
+            print("Asignatura aprobada")
+        elif nota<5 and nota>=0:
+            print("Asignatura suspendida")
+        else: #en caso de no estar entre 0 y 10 salta error
+            print("Error")
 
-
-else: #en caso de no serlo printea error
+else: #en caso de no ser entero printea error
     print("Error")
