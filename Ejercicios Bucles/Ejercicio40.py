9d437f83b3e72dfaefdeee2fb9edbe287c782429
maangeel <miguel.fernandez@campus.xaloc.org>
2025-11-07 15:44:44 +0100

    p

--- Ejercicios Bucles/Ejercicio40.py
+++ Ejercicios Bucles/Ejercicio40.py
@@ -4,8 +4,8 @@
 pares = 0
 impares = 0
 
-for i in range(1,51):
-    if i%2==0:
+for i in range(1,51): #bucle for que recorre todos los números del 1 al 51 sin contar el 51
+    if i%2==0: #si el residuo de i/2 es 0 significa que es par
         pares +=1
     else:
         impares+=1
