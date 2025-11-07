643c3c114c8be4707719c30b456d15b1b2d5c463
maangeel <miguel.fernandez@campus.xaloc.org>
2025-11-07 16:02:19 +0100

    Update Ejercicio42.py

--- Ejercicios Bucles/Ejercicio42.py
+++ Ejercicios Bucles/Ejercicio42.py
@@ -1,10 +1,13 @@
 #42. Imprima el siguiente patrón con el ciclo for:
 
-for i in range(0,5):
-    for j in range(i):
-        print("*", end="")
+#primer bloque
+for i in range(0,5):#le asigna a i el recorrido del 1 al 5
+    for j in range(i): #hace tantas repeticiones como valor de i
+        print("*", end="") #imprime un asterisco y no salta a la siguiente línea
     print("")
-for i in range(5,0,-1):
+
+#segundo bloque
+for i in range(5,0,-1): #hace lo mismo a la inversa
     for j in range(i):
         print("*", end="")
     print("")
