4f1878052135cb5bf917d3de5c5c44d4cb4148c3
maangeel <miguel.fernandez@campus.xaloc.org>
2025-11-11 10:47:27 +0100

    p

--- Ejercicios Bucles/Ejercicio44.py
+++ Ejercicios Bucles/Ejercicio44.py
@@ -1,6 +1,6 @@
 #44. Realiza un programa que recorra todos los números comprendidos de 0 a 100 realizando saltos 
 #de 3 en 3. El resultado debe aparecer por pantalla en una línea con los números separados por ‘,’
 
-for i in range(0,100,3):
+for i in range(0,100,3): #recorre 0 a 100 de 3 en 3
     print(i,end="") #imprime i y manda que se siga el print en la misma línea
     print(", ",end="") #separación por comas
--- Ejercicios Bucles/Ejercicio47.py
+++ Ejercicios Bucles/Ejercicio47.py
@@ -2,3 +2,9 @@
 #mostrar el rango de números teniendo en cuenta que se a<b la secuencia será incremental y si a>b 
 #la secuencia en descenso. Respeta el formato de salida
 
+#inputs
+a = int(input("Introduce el primer valor del intervalo: "))
+b = int(input("Introduce el segundo valor del intervalo: "))
+
+for i in range(a,b+1):
+    print(f"{i}-",end="")
