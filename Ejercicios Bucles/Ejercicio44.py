9ffcad5107c850d4795ceffe758853b2a2da7a3c
maangeel <miguel.fernandez@campus.xaloc.org>
2025-11-12 09:46:05 +0100

    Update Ejercicio44.py

--- Ejercicios Bucles/Ejercicio44.py
+++ Ejercicios Bucles/Ejercicio44.py
@@ -2,5 +2,7 @@
 #de 3 en 3. El resultado debe aparecer por pantalla en una línea con los números separados por ‘,’
 
 for i in range(0,100,3): #recorre 0 a 100 de 3 en 3
-    print(i,end="") #imprime i y manda que se siga el print en la misma línea
-    print(", ",end="") #separación por comas
+    if i != 99: #así se evita poner coma al final de la serie.
+        print(i,end=", ") #imprime i y manda que se siga el print en la misma línea
+    else:
+        print(i)
