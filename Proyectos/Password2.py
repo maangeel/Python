262508a533275bed460c60ae87283e764a59e933
maangeel <miguel.fernandez@campus.xaloc.org>
2025-11-14 15:29:07 +0100

    Create Password2.py

---
+++ Proyectos/Password2.py
@@ -0,0 +1,4 @@
+#verión 2 del password. Con bucles for. Requisitos: tener 3 letras, 3 números y 2 símbolos. 3 entradas de password
+
+#3 inputs almacenados en una lista
+password=[input("Introduce password 1: "),input("Introduce password 2: "),input("Introduce password 3: ")]
