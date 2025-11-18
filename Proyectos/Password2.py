ca242da89840ae5745d17a00338888d860048371
maangeel <miguel.fernandez@campus.xaloc.org>
2025-11-18 10:18:18 +0100

    Update Password2.py

--- Proyectos/Password2.py
+++ Proyectos/Password2.py
@@ -4,8 +4,11 @@ print("INSTRUCCIONES")
 print("1. La longitud del password tiene que tener 8 caracteres")
 print("2. Debe contener 3 letras, 3 números y 2 símbolos")
 
+#inicialización de variables
+correctPassword = 0
+incorrectPassword = 0
 
-letras = 0
+letras = 0  
 num = 0
 simbol = 0
 
@@ -18,13 +21,20 @@ for i in password: #asigna a i cada password
     simbol = 0
     if len(i) == 8:
         for j in range(8):
-            if password[j].isalpha():
+            if i[j].isalpha():
                 letras += 1
-            elif password[j].isdigit():
+            elif i[j].isdigit():
                 num += 1
             else:
                 simbol += 1
-    else:
+    else: #si no cumple requisito de longitud se ahorra comprobar las condiciones de arriba
         print("Longitud incorrecta")
-    if letras == 3 and num == 3 and simbol == 2:
-        
+    if letras == 3 and num == 3 and simbol == 2: #comprueba que el Password sea correcto
+        correctPassword+=1
+        print(f'El Password "{i}" cumple con los requisitos.')
+    else:
+        incorrectPassword+=1
+        print(f'El Password "{i}" no cumple con los requisitos.')
+
+#salida de datos
+print(f"Has introducido {correctPassword} Passwords correctas y {incorrectPassword} Password incorrectas.")
