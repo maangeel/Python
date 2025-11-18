186f4bcd572e1aec20a68a20ad195165fbdb772a
maangeel <miguel.fernandez@campus.xaloc.org>
2025-11-18 10:33:47 +0100

    Update bucle for while.py

--- Ejercicios Guiados/bucle for while.py
+++ Ejercicios Guiados/bucle for while.py
@@ -14,15 +14,44 @@
 #while True: #bucle infinito
 #    print("Hola")
 
-totalV = 0 #para la suma de vocales
-sumaNum = 0 #para la suma de num
-password = "a7e4jk"
+#totalV = 0 #para la suma de vocales
+#sumaNum = 0 #para la suma de num
+#password = "a7e4jk"
+#
+#for i in password:
+#    if i.lower() in "aeiou":
+#        totalV += 1
+#    if i.isdigit():
+#        sumaNum = int(sumaNum)
+#        sumaNum += int(i)
+#
+#print(f"Hay {totalV} vocales y la suma total es {sumaNum}.")
 
-for i in password:
-    if i.lower() in "aeiou":
-        totalV += 1
-    if i.isdigit():
-        sumaNum = int(sumaNum)
-        sumaNum += int(i)
+#bucle while:
 
-print(f"Hay {totalV} vocales y la suma total es {sumaNum}.")
+import random
+#numAleatorio = random.randint(1,20)
+#num = 0
+#
+#while num != numAleatorio:
+#    num = int(input("Introduce un valor entre 1 y 20: "))
+#    if num == numAleatorio:
+#        print("Acertaste")
+#        break
+#    else:
+#        print("Incorrecto")
+
+#version2
+
+numAleatorio = random.randint(1,20)
+num = 0
+intentos = 0
+
+while num != numAleatorio and intentos != 3:
+    num = int(input("Introduce un valor entre 1 y 20: "))
+    if num == numAleatorio:
+        print("Acertaste")
+    else:
+        print("Incorrecto")
+        intentos+=1
+print(f"El número era {numAleatorio}.")
