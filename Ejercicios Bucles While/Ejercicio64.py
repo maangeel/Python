cd0dccac223662bc2089c96e20b8666c775e04ba
maangeel <miguel.fernandez@campus.xaloc.org>
2025-12-02 10:18:21 +0100

    Update Ejercicio64.py

---
+++ Ejercicios Bucles While/Ejercicio64.py
@@ -0,0 +1,39 @@
+#64. Programa que pida continuamente números por teclado hasta que el usuario introduzca el valor -99.
+
+val = int(input("Introduce un valor: ")) #input
+
+#variables
+pares = 0
+impares = 0
+positivos = 0
+negativos = 0
+ceros = 0
+sumaTotal = 0
+
+while val != -99:
+    if val%2==0:
+        pares+=1 #comprueba pares
+    elif val==0:
+        ceros+=1 #comprueba ceros
+    else:
+        impares+=1 #comprueba impares
+    if val>0:
+        positivos+=1 #comprueba positivos
+    if val<0:
+        negativos+=1 #comprueba negativos
+    sumaTotal+=val#suma total
+    val = int(input("Introduce un valor: "))
+    
+print("RESUMEN")
+#los prints solo aparecerán si el valor es mayor que 0
+if pares>0:
+    print(f"El número de pares es {pares}")
+if impares>0:
+    print(f"El número de impares es {impares}")
+if positivos>0:
+    print(f"El número de positivos es {positivos}")
+if negativos>0:
+    print(f"El número de negativos es {negativos}")
+if ceros>0:
+    print(f"El número de ceros es {ceros}")
+print(f"El total es {sumaTotal}")
