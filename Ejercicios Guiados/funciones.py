1322a03d7a57267ba31724edffff9aee1f5a88b8
maangeel <miguel.fernandez@campus.xaloc.org>
2025-12-09 10:48:03 +0100

    `p

---
+++ Ejercicios Guiados/funciones.py
@@ -0,0 +1,11 @@
+#declaración de funciones
+
+#def imprimir(nombre=""):
+#    print(nombre)
+#
+#imprimir("Hola mundo")
+
+#def suma(a,b):
+#    return print(f"El resultado de la suma es {a+b}")
+#suma(int(input("Introduce 1 valor: ")),int(input("Introduce otro valor: ")))
+
