ee413018d72930d8121e917d00b1e69df0bec559
maangeel <miguel.fernandez@campus.xaloc.org>
2025-12-10 09:54:40 +0100

    Update funciones.py

--- Ejercicios Personales/funciones.py
+++ Ejercicios Personales/funciones.py
@@ -1,2 +1,88 @@
-def calc(inputName):
+opcion = 0
+resultado = 0
+continuar = ""
+#función introducir un valor
+def introValor(inputMessage):
+    while True:
+        try:
+            return float(input(inputMessage))
+        except ValueError:
+            print("El valor introducido es incorrecto")
+
+#función interfaz del menú
+def menu():
+    print("\n--- Calculadora ---")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Salir")
+
+#continuar programa
+def continuarPrograma(mensage):
+    while True:
+        try:
+            continuar = input(mensage)
+            if continuar.lower() == "s":
+                main()
+            elif continuar.lower() == "n":
+                print("Finalizando")
+                break
+        except ValueError:
+            print("Error")
+            break
+
+#función suma
+def suma(a,b):
+    return a+b
+
+#función resta
+def resta(a,b):
+    return a-b
+
+#función multiplicación
+def multi(a,b):
+    return a*b
+
+#función división
+def divi(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        print("Syntax Error")
     
+def main():
+    while True:
+        menu() #muestra menú
+        opcion = input("Introduce una opción: ")
+
+        if opcion == 5: #opción salir
+            break
+        if opcion not in ("1","2","3","4"):
+            print("Opción no válida")
+            continue #vuelve al bucle
+
+        #inputs
+        num1 = introValor("Introduce un valor: ")
+        num2 = introValor("Introduce otro valor: ")
+
+        try: #opciones comprobación de error y suma
+            if opcion == "1":
+                resultado = suma(num1,num2)
+                print(resultado)
+            if opcion == "2":
+                resultado = resta(num1,num2)
+                print(resultado)
+            if opcion == "3":
+                resultado = multi(num1,num2)
+                print(resultado)
+            if opcion == "4":
+                resultado = divi(num1,num2)
+                print(resultado)
+        except ValueError as e:
+            print(f"Syntax Error, {e}")
+        continuarPrograma("¿Quieres continuar el programa? s/n: ")
+        
+
+#inicio del programa
+continuarPrograma("¿Quieres iniciar el programa? s/n: ")
