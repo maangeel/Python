3925c259261853e6b296cafdf6465f5ab0bb917c
maangeel <miguel.fernandez@campus.xaloc.org>
2026-01-27 09:50:24 +0100

    p

--- Ejercicios Listas/Ejercicio74.py
+++ Ejercicios Listas/Ejercicio74.py
@@ -1,6 +1,6 @@
-#73. Diseña un programa que compruebe si los valores de la lista1 (casa,mesa,sal,sol,agua) están 
-#repetidos o no en la lista2 (casa,luz,tres,tren,sol,pan). Haz que permita visualizar que palabras se 
-#repiten y cuales no
+#74. A partir del programa anterior, haz que se visualicen tanto las palabras que se repiten o no de 
+#entre las 2 listas.
+
 
 lista1 = ["casa","mesa","sal","sol","agua"]
 lista2 = ["casa","luz","tres","tren","sol","pan"]
@@ -12,6 +12,12 @@ for i in lista2:
         repetidas.append(i)
     else:
         noRepetidas.append(i)
+
+for j in lista1:
+    if j in lista2 and j not in repetidas:
+        repetidas.append(j)
+    elif j not in lista2 and j not in noRepetidas:
+        noRepetidas.append(j)
 
 
 print("Están repetidas:",repetidas)
