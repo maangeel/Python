b92492bc74009fc280a1ca9b9e40f7bbef6fbf21
maangeel <miguel.fernandez@campus.xaloc.org>
2026-01-28 09:07:23 +0100

    Update Ejercicio81.py

--- Ejercicios Listas/Ejercicio81.py
+++ Ejercicios Listas/Ejercicio81.py
@@ -1,5 +1,6 @@
 #81. A partir de una lista definida, busca el método apropiado para que se visualice un valor de la 
 #lista al azar.
+
 import random
 lista = ["casa","barco","gato","perro","madera","agua","puente","pantalón"]
 print(random.choice(lista)) #escoge un valor al azar
