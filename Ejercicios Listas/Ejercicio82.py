be06efb732f0929ac345e4171e1e900b6c5ca449
maangeel <miguel.fernandez@campus.xaloc.org>
2026-01-28 09:10:16 +0100

    Update Ejercicio82.py

--- Ejercicios Listas/Ejercicio82.py
+++ Ejercicios Listas/Ejercicio82.py
@@ -2,3 +2,18 @@
 #azar de la lista, indicando si es correcto o no. El programa debe no finaliza hasta que no se adivine 
 #la palabra
 
+import random
+
+lista = ["casa","barco","gato","perro","madera","agua","puente","pantalón"]
+print(lista)
+
+palabra = random.choice(lista) #escoge un valor al azar
+
+adivina = input("Introduce la palabra secreta: ")
+
+while adivina != palabra:
+    print("SIGUE JUGANDO")
+    adivina = input("Introduce la palabra secreta: ")
+
+else:
+    print("ACERTASTE")
