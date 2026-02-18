b0c0e467ba0f37171da174b9c07062bca709b210
maangeel <miguel.fernandez@campus.xaloc.org>
2026-02-18 09:48:38 +0100

    Update Ahorcado.py

--- Proyectos/Ahorcado.py
+++ Proyectos/Ahorcado.py
@@ -12,24 +12,38 @@ intentos = 0
 
 #randomizar palabra
 def randomizar_palabra():
-    global palabra_secreta #variable global para usarla en otras funciones
-    palabra_secreta = random.choice(lista_palabrasecreta)
+    global palabra_secreta
     global lista_partida
+    palabra_secreta = random.choice(lista_palabrasecreta)
     lista_partida = ["_"] * len(palabra_secreta)
     print(",".join(lista_partida)) #join para quitar lista y mostrar solo la palabra con comas
-    global lista_ahorcado
-    lista_ahorcado = []
     global intentos
     intentos = 0
+    lista_palabrasecreta.remove(palabra_secreta)
 
 #añadir letras a la partida
 def agregar_letra(letra):
+    global lista_partida
     if letra in palabra_secreta:
-        for i in range(len(palabra_secreta)):
+        for i in range(len(palabra_secreta)): #recorre la palabra secreta para encontrar la letra y sustituirla en la lista partida
             if palabra_secreta[i] == letra:
                 lista_partida[i] = letra
     else:
-        lista_ahorcado.append(letras_ahorcado[intentos])
         global intentos
+        lista_ahorcado.append(letras_ahorcado[intentos]) #sustituye la letra por la letra del ahorcado correspondiente al número de intentos
         intentos += 1
+    print(",".join(lista_partida))
 
+def partida(): #función para jugar la partida
+    while lista_ahorcado != letras_ahorcado:
+        agregar_letra(input("Ingresa una letra: ").lower())
+    if "_" not in lista_partida:
+        print("¡Felicidades! Has ganado. La palabra era:", palabra_secreta)
+    else:
+        print("¡Has perdido! La palabra era:", palabra_secreta)
+    print(",".join(lista_partida))
+
+
+#PARTIDA
+randomizar_palabra()
+partida()
