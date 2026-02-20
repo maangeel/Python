40582f95ea877da61717b07831d10d9e60c7fcb5
maangeel <miguel.fernandez@campus.xaloc.org>
2026-02-20 15:42:38 +0100

    Update Ahorcado.py

--- Proyectos/Ahorcado.py
+++ Proyectos/Ahorcado.py
@@ -10,33 +10,34 @@ letras_ahorcado = ["A", "H", "O", "R", "C", "A", "D", "O"]
 intentos = 0
 
 
-#randomizar palabra
-def randomizar_palabra():
-    global palabra_secreta
-    global lista_partida
-    palabra_secreta = random.choice(lista_palabrasecreta)
+#Función para randomizar la palabra secreta
+def randomizar_palabra(listaPalabras):
+    palabra_secreta = random.choice(listaPalabras)
     lista_partida = ["_"] * len(palabra_secreta)
-    print(",".join(lista_partida)) #join para quitar lista y mostrar solo la palabra con comas
-    global intentos
+    print(",".join(lista_partida))
     intentos = 0
     lista_palabrasecreta.remove(palabra_secreta)
+    return palabra_secreta, lista_partida, intentos, lista_palabrasecreta
+
 
 #añadir letras a la partida
 def agregar_letra(letra):
-    global lista_partida
     if letra in palabra_secreta:
-        for i in range(len(palabra_secreta)): #recorre la palabra secreta para encontrar la letra y sustituirla en la lista partida
+        for i in range(len(palabra_secreta)): #recorre la palabra para comprobar la letra
             if palabra_secreta[i] == letra:
-                lista_partida[i] = letra
+                lista_partida[i] = letra #sustituye el guion bajo por la letra correcta
     else:
         global intentos
-        lista_ahorcado.append(letras_ahorcado[intentos]) #sustituye la letra por la letra del ahorcado correspondiente al número de intentos
+        lista_ahorcado.append(letras_ahorcado[intentos]) #añade una letra a AHORCADO
         intentos += 1
     print(",".join(lista_partida))
+    print(" ".join(lista_ahorcado)) #imprime la palabra ahorcado
+    return lista_partida, lista_ahorcado
 
-def partida(): #función para jugar la partida
+
+def partida(lista_partida, lista_ahorcado): #función para jugar la partida
     while lista_ahorcado != letras_ahorcado:
-        agregar_letra(input("Ingresa una letra: ").lower())
+        lista_partida, lista_ahorcado = agregar_letra(input("Ingresa una letra: ").lower())
     if "_" not in lista_partida:
         print("¡Felicidades! Has ganado. La palabra era:", palabra_secreta)
     else:
@@ -45,5 +46,6 @@ def partida(): #función para jugar la partida
 
 
 #PARTIDA
-randomizar_palabra()
-partida()
+palabra_secreta, lista_partida, intentos, lista_palabrasecreta = randomizar_palabra(lista_palabrasecreta)
+lista_partida, lista_ahorcado = agregar_letra(input("Ingresa una letra: ").lower())
+partida(lista_partida, lista_ahorcado)
