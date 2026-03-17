dd98a82e74e8fe949e308e69756d6c287a4c7479
maangeel <miguel.fernandez@campus.xaloc.org>
2026-03-17 10:28:33 +0100

    Update Juego7yMedioFASE1.py

--- Proyectos/Juego7yMedioFASE1.py
+++ Proyectos/Juego7yMedioFASE1.py
@@ -7,9 +7,11 @@ listaCartas = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,10,10,10,
 #valor de las cartas
 valoresCartas = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5]
 
+totalValores = 0
+
 #función para obtener una carta y su valor
 def obtenerCarta():
-    respuesta = input("¿Quieres una carta? (s/n): ")
+    respuesta = input("\n¿Quieres una carta? (s/n): ")
     if respuesta == "s" and len(listaCartas) > 0:
         numeroCarta = random.randint(0,len(listaCartas)-1)
         valorCarta = valoresCartas[numeroCarta] #asignar el valor de la carta
@@ -21,18 +23,32 @@ def obtenerCarta():
         return signoCarta, valorCarta, listaCartas, valoresCartas
     while respuesta != "s" and respuesta != "n":
         print("Por favor, ingresa 's' o 'n'.")
-        respuesta = input("¿Quieres una carta? (s/n): ")
+        respuesta = input("\n¿Quieres una carta? (s/n): ")
         
     else:
-        return 0,0
+        return 0,0, listaCartas, valoresCartas
 
 print("Bienvenido al juego de 7 y Medio")
 
 #obtener la primera carta
 carta, valor, listaCartas, valoresCartas = obtenerCarta()
 print(f"Has obtenido la carta {carta} con valor {valor}")
+totalValores += valor
+print(f"Tu total actual es: {totalValores}")
 
 #resto de cartas
-while carta != 0 and valor != 0:
+while carta != 0 and valor != 0 and totalValores < 7.5:
     carta, valor, listaCartas, valoresCartas = obtenerCarta() #se actualizan los valores y listas
     print(f"Has obtenido la carta {carta} con valor {valor}")
+    totalValores += valor
+    print(f"Tu total actual es: {totalValores}")
+
+else:
+    if totalValores > 7.5:
+        print("Has perdido la partida")
+    elif totalValores == 7.5:
+        print("Enhorabuena, has ganado la partida")
+    elif totalValores >=6 and totalValores <= 7:
+        print(f"Has sido un poco conservador")
+    else:
+        print("Quizás tendrías que haber arriesgado un poco, ¿No?")
